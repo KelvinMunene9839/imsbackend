a464ac48c61b58c9d314b3fa54173eec910660f8
nyiringabocaleb26 <calebnyiringabo26@gmail.com>
2025-06-24 09:46:18 +0200

    second

--- frontend/lib/pages/investor/investor_dashboard.dart
+++ frontend/lib/pages/investor/investor_dashboard.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'overview_tab.dart';
+import 'contributions_tab.dart';
+import 'interest_tab.dart';
+import 'rankings_tab.dart';
 
 class InvestorDashboard extends StatefulWidget {
   const InvestorDashboard({super.key});
