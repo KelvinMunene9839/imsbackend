5fba48d55af361543637c1f4a13b12c96f31a2b0
kelvinmunene9839 <kelvinnyagah9839@gmail.com>
2025-07-21 07:11:02 +0200

    q

--- frontend/lib/pages/admin/assets_tab.dart
+++ frontend/lib/pages/admin/assets_tab.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:frontend/config.dart';
-import '../../widgets/stat_card.dart';
 
 class AssetsTab extends StatefulWidget {
   const AssetsTab({super.key});
