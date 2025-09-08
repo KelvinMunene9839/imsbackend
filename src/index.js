0d9670da7c1fa72a8292c7bab26b1ef49529dddc
kelvinmunene9839 <kelvinnyagah9839@gmail.com>
2025-09-08 17:37:54 +0200

    new commit

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ import reportRoutes from './reportRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000', // React frontend URL
+  credentials: true
+}));
 app.use(express.json({ extended: true }));
 
 
