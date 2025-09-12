71d2695253fa7c928612ef65931a1ec67459117b
kelvinmunene9839 <kelvinnyagah9839@gmail.com>
2025-09-12 19:03:46 +0200

    new

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000', // React frontend URL
+  origin: 'http://localhost:5173', // React frontend URL
   credentials: true
 }));
 app.use(express.json({ extended: true }));
